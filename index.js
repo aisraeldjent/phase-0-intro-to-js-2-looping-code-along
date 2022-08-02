9d3044253fc0ee795fc13711b9c64f9f9daa2cd4
Aidan Israel <aidandisrael@gmail.com>
2022-08-02 16:47:43 -0700

    writecards and countdown

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, adjective) {
+    var messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${adjective} gift!`);
+    }
+    return messages;
+}
+
+function countDown(count) {
+    while ( count != -1) {
+        console.log(count);
+        count = count -1;
+    }
+}
